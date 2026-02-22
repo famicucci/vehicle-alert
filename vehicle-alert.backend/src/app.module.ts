8c7e48d1b4e8e59638970312b69da43b38a3aeb9
Francisco A. Micucci <francisco.micucci@diveria.com>
2026-02-22 19:04:14 -0300

    delete test modules

--- vehicle-alert.backend/src/app.module.ts
+++ vehicle-alert.backend/src/app.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import config from './typeorm.config';
 import { TestCasesModule } from './test-cases/test-cases.module';
-import { TestsModule } from './tests/tests.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(config), TestCasesModule, TestsModule],
+  imports: [TypeOrmModule.forRoot(config), TestCasesModule],
   controllers: [],
   providers: [],
 })
