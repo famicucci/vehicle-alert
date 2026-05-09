4449b267995f587a35b918604f4325da32daa34a
Francisco Micucci <francisco.micucci@marvik.ai>
2026-05-09 19:04:37 -0300

    Refactor Home component to redirect to the vehicle search page, removing the previous layout and links for a streamlined user experience.

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,5 @@
-import Image from "next/image";
-import Link from "next/link";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <>
-      <div className="flex min-h-screen flex-col items-center justify-center p-24 gap-8">
-        <h1 className="text-4xl font-bold text-center">Alerta Vehículos</h1>
-        <div className="flex gap-4">
-          <Link
-            href="/buscar-vehiculo"
-            className="px-4 py-2 bg-green-500 text-white text-center rounded hover:bg-green-600 transition"
-          >
-            Buscar Vehículos
-          </Link>
-        </div>
-      </div>
-    </>
-  );
+  redirect("/buscar-vehiculo");
 }
